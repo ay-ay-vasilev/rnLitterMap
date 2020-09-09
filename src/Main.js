0885256ccf727912367874aaa32c782b82990e4d
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-09 13:29:24 +0900

    Implemented react navigation: bottom tab menu and two screens

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,48 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
+
+import "react-native-gesture-handler";
+
+import { StatusBar } from "expo-status-bar";
 import MapView from "react-native-maps";
 
-export default function Main() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function MapScreen() {
   return (
     <View style={styles.container}>
       <MapView style={styles.mapStyle} />
     </View>
+  );
+}
+
+function CardScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Something here</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function Main() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Cards"
+          component={CardScreen}
+          options={{ title: "Litter Cards" }}
+        />
+        <Tab.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ title: "Map" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
