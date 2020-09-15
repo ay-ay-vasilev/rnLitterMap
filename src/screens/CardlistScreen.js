a6d4f47f3b5c175ca68c2bedb544c68cd8162248
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-15 20:30:22 +0900

    Prepared variables for putting real data

--- src/screens/CardlistScreen.js
+++ src/screens/CardlistScreen.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
 import { List } from "react-native-paper";
-
 import styles from "../styles/styles";
-
 import RaisedButton from "../components/RaisedButton";
 
 function showImage1() {
@@ -15,42 +13,83 @@ function showImage1() {
   );
 }
 
-function showImage2() {
-  return (
-    <Image
-      source={require("../assets/trash_2.jpg")}
-      style={{ width: 85, height: 85 }}
-    />
-  );
-}
-
 export default function CardlistScreen({ navigation }) {
+  const fakeData = [
+    {
+      cleaned: true,
+      distance: "5 км",
+      size: "small",
+      date: "5 дней назад",
+      img: "organic_trash.jpg",
+    },
+    {
+      cleaned: true,
+      distance: "5 км",
+      size: "small",
+      date: "5 дней назад",
+      img: "organic_trash.jpg",
+    },
+    {
+      cleaned: true,
+      distance: "5 км",
+      size: "small",
+      date: "5 дней назад",
+      img: "organic_trash.jpg",
+    },
+    {
+      cleaned: true,
+      distance: "5 км",
+      size: "small",
+      date: "5 дней назад",
+      img: "organic_trash.jpg",
+    },
+    {
+      cleaned: true,
+      distance: "5 км",
+      size: "small",
+      date: "5 дней назад",
+      img: "organic_trash.jpg",
+    },
+    {
+      cleaned: true,
+      distance: "5 км",
+      size: "small",
+      date: "5 дней назад",
+      img: "organic_trash.jpg",
+    },
+    {
+      cleaned: true,
+      distance: "5 км",
+      size: "small",
+      date: "5 дней назад",
+      img: "organic_trash.jpg",
+    },
+  ];
+
+  const dumpCards = fakeData.map((element) => (
+    <List.Item
+      titleNumberOfLines={3}
+      title={
+        <Text>
+          <Text style={styles.cardListTitle1Cleaned}>
+            {element.cleaned ? "Убрано" : "Не убрано"}
+            {`\n`}
+          </Text>
+          <Text style={styles.cardListTitle2}>
+            {element.distance}
+            {`\n`}
+          </Text>
+          <Text style={styles.cardListTitle3}>{element.size}</Text>
+        </Text>
+      }
+      description="5 дней назад"
+      left={showImage1}
+    />
+  ));
+
   return (
     <View style={{ flex: 1 }}>
-      <List.Item
-        titleNumberOfLines={3}
-        title={
-          <Text>
-            <Text style={styles.cardListTitle1Cleaned}>Убрано{`\n`}</Text>
-            <Text style={styles.cardListTitle2}>в 5 км{`\n`}</Text>
-            <Text style={styles.cardListTitle3}>Хватит одного пакета</Text>
-          </Text>
-        }
-        description="5 дней назад"
-        left={showImage1}
-      />
-      <List.Item
-        titleNumberOfLines={3}
-        title={
-          <Text>
-            <Text style={styles.cardListTitle1NotCleaned}>Не убрано{`\n`}</Text>
-            <Text style={styles.cardListTitle2}>в 100 м{`\n`}</Text>
-            <Text style={styles.cardListTitle3}>Нужно несколько пакетов</Text>
-          </Text>
-        }
-        description="Неделю назад"
-        left={showImage2}
-      />
+      {dumpCards}
       <View style={styles.buttonBottomRaised}>
         <RaisedButton navigation={navigation} path="Cardlist" text="Добавить" />
       </View>
