e88e0878e4ee7123bec969d4c7c55bdea1dea84b
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-16 20:49:48 +0900

    Account screen layouting (just started)

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,13 +1,53 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Avatar, Card, Title, Paragraph } from "react-native-paper";
+
 import RaisedButton from "../components/RaisedButton";
 import styles from "../styles/styles";
 
 export default function AccountScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Account Screen</Text>
-      <RaisedButton navigation={navigation} path="Account" text="Сохранить" />
+    <View>
+      <View style={{ alignItems: "center", marginTop: 30, marginBottom: 30 }}>
+        <Avatar.Image size={172} source={require("../assets/no-img.png")} />
+      </View>
+
+      <View style={{ margin: 10 }}>
+        <Card>
+          <Card.Content>
+            <Title>Test</Title>
+            <Paragraph>Testtest</Paragraph>
+          </Card.Content>
+        </Card>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Card>
+          <Card.Content>
+            <Title>Test</Title>
+            <Paragraph>Testtest</Paragraph>
+          </Card.Content>
+        </Card>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Card>
+          <Card.Content>
+            <Title>Test</Title>
+            <Paragraph>Testtest</Paragraph>
+          </Card.Content>
+        </Card>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Card>
+          <Card.Content>
+            <Title>Test</Title>
+            <Paragraph>Testtest</Paragraph>
+          </Card.Content>
+        </Card>
+      </View>
+
+      <View style={{ alignItems: "center", marginTop: 30 }}>
+        <RaisedButton navigation={navigation} path="Account" text="Сохранить" />
+      </View>
     </View>
   );
 }
