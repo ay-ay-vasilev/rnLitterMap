3f528b98f8d690fdaa3fa26e513fb66b04277fc2
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-16 16:00:44 +0900

    Created a component and moved it out from the screen

---
+++ src/components/ListItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Image, Text } from "react-native";
+import { List } from "react-native-paper";
+import styles from "../styles/styles";
+import { translateSize } from "../utils/utils";
+
+export default function ListItem(props) {
+  return (
+    <List.Item
+      titleNumberOfLines={3}
+      title={
+        <Text>
+          <Text
+            style={
+              props.element.cleaned
+                ? styles.cardListTitle1Cleaned
+                : styles.cardListTitle1NotCleaned
+            }
+          >
+            {props.element.cleaned ? "Убрано" : "Не убрано"}
+            {`\n`}
+          </Text>
+          <Text style={styles.cardListTitle2}>
+            {props.element.distance}
+            {`\n`}
+          </Text>
+          <Text style={styles.cardListTitle3}>
+            {translateSize(props.element.size)}
+          </Text>
+        </Text>
+      }
+      description={props.element.date}
+      left={() => (
+        <Image source={props.element.img} style={{ width: 85, height: 85 }} />
+      )}
+    />
+  );
+}
--- src/screens/CardlistScreen.js
+++ src/screens/CardlistScreen.js
@@ -1,43 +1,14 @@
 import React from "react";
-import { View, Image, Text, ScrollView } from "react-native";
-import { List } from "react-native-paper";
+import { View, ScrollView } from "react-native";
 import styles from "../styles/styles";
 import RaisedButton from "../components/RaisedButton";
+import ListItem from "../components/ListItem";
 
-import { translateSize } from "../utils/utils";
 import fakeData from "../test/testData";
 
 export default function CardlistScreen({ navigation }) {
   const dumpCards = fakeData.map((element, index) => (
-    <List.Item
-      key={index}
-      titleNumberOfLines={3}
-      title={
-        <Text>
-          <Text
-            style={
-              element.cleaned
-                ? styles.cardListTitle1Cleaned
-                : styles.cardListTitle1NotCleaned
-            }
-          >
-            {element.cleaned ? "Убрано" : "Не убрано"}
-            {`\n`}
-          </Text>
-          <Text style={styles.cardListTitle2}>
-            {element.distance}
-            {`\n`}
-          </Text>
-          <Text style={styles.cardListTitle3}>
-            {translateSize(element.size)}
-          </Text>
-        </Text>
-      }
-      description={element.date}
-      left={() => (
-        <Image source={element.img} style={{ width: 85, height: 85 }} />
-      )}
-    />
+    <ListItem key={index} element={element} />
   ));
 
   return (
