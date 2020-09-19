0bbdb9c35f9e9efb93a806753f5780f5cae7c73d
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-19 20:18:34 +0900

    Tiny fixes

--- src/screens/AddTrashCardScreen.js
+++ src/screens/AddTrashCardScreen.js
@@ -4,13 +4,8 @@ import { useTheme, RadioButton } from "react-native-paper";
 
 import styles from "../styles/styles";
 // Custom components
-import InfoList from "../components/InfoList";
-import UserCard from "../components/UserCard";
-import { GradientHighlightGreen } from "../components/GradientHighlight";
 import { PhotoMenuNotCleaned } from "../components/PhotoMenu";
 import RaisedButton from "../components/RaisedButton";
-
-import { fakeUser } from "../test/testData";
 
 export default function AddTrashCardScreen({ navigation }) {
   const { colors } = useTheme();
@@ -31,15 +26,15 @@ export default function AddTrashCardScreen({ navigation }) {
           alignItems: "center",
         }}
       >
-        <GradientHighlightGreen />
         <View
           style={{
             alignItems: "center",
             justifyContent: "center",
             marginBottom: 20,
+            marginTop: 20,
           }}
         >
-          <Text style={{ fontSize: 14, fontWeight: "bold" }}>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
             Размер мусора
           </Text>
         </View>
