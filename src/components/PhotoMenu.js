04d3d229a24f03911d94cd33dc01b2cc600ddffa
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-20 17:23:13 +0900

    Added an image picker for the trash card adding screen

--- src/components/PhotoMenu.js
+++ src/components/PhotoMenu.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Image, Text } from "react-native";
 import styles from "../styles/styles";
 import { GradientBlack } from "../components/Gradients";
 
 import BackButton from "../components/BackButton";
 import CustomFAB from "../components/CustomFAB";
+
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 export function PhotoMenuNotCleaned(props) {
   return (
@@ -30,11 +33,47 @@ export function PhotoMenuNotCleaned(props) {
 }
 
 export function PhotoMenuUpload(props) {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    getPermission();
+  });
+
+  async function pickImg() {
+    // Pick an image
+
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    } catch (E) {
+      console.log(E);
+    }
+  }
+
+  async function getPermission() {
+    if (Platform.OS !== "web") {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert(
+          "Для выбора файла, приложению требуется разрешение на использование камеры."
+        );
+      }
+    }
+  }
+
   return (
     <View style={styles.containerBlack}>
       <View style={styles.containerTransparent}>
         <Image
-          source={require("../assets/no-img-uploaded.png")}
+          source={
+            image ? { uri: image } : require("../assets/no-img-uploaded.png")
+          }
           style={styles.containerBlack}
         />
         <GradientBlack />
@@ -43,7 +82,7 @@ export function PhotoMenuUpload(props) {
         <CustomFAB
           icon="plus"
           style={styles.fabButtonBig}
-          onPress={() => goToInitialLocation()}
+          onPress={() => pickImg()}
         />
       </View>
       <View
